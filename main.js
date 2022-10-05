0f3eb4fe1d2ab5bd5831a6d9ea3755e437e2f051
ylahssini@gmail.com <ylahssini@gmail.com>
2022-10-05 09:45:24 +0100

    add fullscreen script

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-console.log('Hello World!');
+const main = document.getElementById('main');
+const startTesting = document.getElementById('start-testing');
+const fullscreen = document.getElementById('fullscreen');
+const element = document.documentElement;
+
+const colors = ['white', 'black', 'red', 'green', 'blue'];
+
+function detectDoubleTapClosure() {
+  let lastTap = 0;
+  let timeout;
+
+  return function detectDoubleTap(event) {
+    const curTime = new Date().getTime();
+    const tapLen = curTime - lastTap;
+    if (tapLen < 500 && tapLen > 0) {
+      if (callback) {
+        callback();
+      }
+      event.preventDefault();
+    } else {
+      timeout = setTimeout(() => {
+        clearTimeout(timeout);
+      }, 500);
+    }
+    lastTap = curTime;
+  };
+}
+
+startTesting.addEventListener('click', () => {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+    return true;
+  }
+  
+  if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+    return true;
+  }
+  
+  if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+    return true;
+  }
+
+  return false;
+});
+
+fullscreen.addEventListener('click', () => {
+  
+});
